e9f3e8e0cf8b0e96996537ce995221a2ae7ab864
Caleb Fahlgren <cfahlgren1@gmail.com>
2020-05-02 18:40:59 -0500

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <h1 class="title is-1 has-text-centered">UV Index</h1>
+      <h1 class="title is-4 has-text-centered has-text-grey-light">Auburn, AL</h1>
+    <div class="circle">
+      <CircleComponent
+              :lat="lat"
+              :lng="lng"
+              :uv_max="uv_max"
+              :uv="uv"/>
+      <p><b>Max UV: {{uv_max}}</b></p>
+      <strong>Time: {{max_uv_time}}</strong>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import VueGeolocation from 'vue-browser-geolocation';
+  import Vue from 'vue'
+  import axios from 'axios'
+  import VueAxios from 'vue-axios'
+  import Buefy from 'buefy'
+  import 'buefy/dist/buefy.css'
+  import CircleComponent from "./components/CircleComponent";
+
+  Vue.use(Buefy)
+  Vue.use(VueAxios, axios)
+  Vue.use(VueGeolocation);
+  Vue.use(Buefy)
+  Vue.use(VueAxios, axios)
 
 export default {
   name: 'App',
+  data() {
+    return {
+      info: null,
+      uv: 0,
+      uv_max: 10,
+      lng: -81.64,
+      lat: 30.12,
+      max_uv_time : null
+    }
+  },
   components: {
-    HelloWorld
+    CircleComponent,
+  },
+  created(){},
+  mounted() {
+    this.$getLocation({enableHighAccuracy: false, timeout: Infinity, maximumAge: 0}).then(coordinates => {
+      this.lng = coordinates.lng;
+      this.lat = coordinates.lat;
+      this.updateUV();
+    });
+  },
+  methods :
+  {
+      updateUV() {
+        var d = new Date();
+        var n = d.toISOString();
+        axios.get('https://api.openuv.io/api/v1/uv?lat=' + this.lat + '&lng=' + this.lng + '&dt=' + n, {
+          params: {},
+          headers: {'x-access-token': '65d941e9fdd0669b2f79c535acbf933b'}
+        }).then(response => {
+          this.info = response.data.result;
+          this.uv = parseFloat(response.data.result['uv'].toFixed(1));
+          this.uv_max = parseFloat(response.data.result['uv_max'].toFixed(1));
+          this.max_uv_time = new Date(response.data.result['uv_max_time']).toLocaleTimeString('en-US');
+        });
+      }
   }
 }
 </script>
---
+++ src/components/CircleComponent.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <radial-progress-bar style="margin: auto;width: 50%;"
+                         :diameter="250"
+                         :inner-stroke-color="innerStrokeColor"
+                         :stroke-width="35"
+                         :completed-steps="uv"
+                         :startColor = "color"
+                         :stopColor = "color"
+                         :total-steps="uv_max">
+      <h1 class="title is-1">{{uv}}</h1>
+    </radial-progress-bar>
+  </div>
+</template>
+
+<script>
+  import VueGeolocation from 'vue-browser-geolocation';
+  import Vue from 'vue'
+  import axios from 'axios'
+  import VueAxios from 'vue-axios'
+  import RadialProgressBar from 'vue-radial-progress'
+  import Buefy from 'buefy'
+  import 'buefy/dist/buefy.css'
+
+  Vue.use(Buefy)
+  Vue.use(VueAxios, axios)
+  Vue.use(VueGeolocation);
+  Vue.use(Buefy)
+  Vue.use(VueAxios, axios)
+
+
+export default {
+  name: 'CircleComponent',
+  data() {
+    return {
+      innerStrokeColor: '#278EA5',
+      color: '#21E6C1',
+      max_uv_time: null
+    }
+  },
+  components: {
+    RadialProgressBar
+  },
+  props: {
+    lng: Number,
+    lat: Number,
+    uv_max: Number,
+    uv: Number,
+
+  },
+  methods :
+          {},
+}
+</script>
+
+<style scoped>
+</style>
